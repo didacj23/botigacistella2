365873bcc1f276c3790997e7524a01d8fb2fc762
Arnau Gispert <arnau.gispert.coll@gmail.com>
2023-03-16 09:06:48 +0100

    Create Botiga.cs

---
+++ botigacistella2/Botiga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace botigacistella2
+{
+    internal class Botiga
+    {
+        public string NomBotiga;
+        public Producte[] Productes;
+        public int NumElements;
+
+
+        //Constructors
+
+
+
+        //Constructors
+
+        //Propietats
+
+
+
+    }
+}
+
