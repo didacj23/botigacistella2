f070a13128cc09adf0c2bd1a51517349ced5af66
Arnau Gispert <arnau.gispert.coll@gmail.com>
2023-03-20 21:30:12 +0100

    Métodes acabats
    falta corregir tostring

--- botigacistella2/Botiga.cs
+++ botigacistella2/Botiga.cs
@@ -106,16 +106,188 @@ namespace botigacistella2
             }
         }
 
-        private 
+        public bool AfegirProduct(Producte producte, Producte[] productes, int NumElements)
+        {
+            int index;
+            
+
+            if(EsapiLliure(productes) == -1)
+            {
+                return false;
+            }
+            else
+            {
+                index = EsapiLliure(productes);
+                productes[index] = producte;
+                return true;
+                NumElements++;
+                
+
+            }
+        }
+
+        //falta inserir conjunt de productes
+
+        private void AmpliarBotiga(int NombreProductes, ref Producte[] productes)
+        {
+            NombreProductes++;
+            productes = new Producte[NombreProductes];
+        }
+
+        public bool ModificarPreu(string producte, double preu)
+        {
+            for (int i = 0; i < NumElements; i++)
+            {
+                if (productes[i].Nom == producte)
+                {
+                    productes[i].PreuSenseIva = preu;
+                    return true;
+                }
+            }
+            Console.WriteLine("El producte no s'ha trobat.");
+            return false;
+        }
+
+
+        public bool BuscarProducte(Producte producte)
+        {
+            for (int i = 0; i < NumElements; i++)
+            {
+                if (productes[i].Nom == producte.Nom)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ModificarProducte(Producte producte, string nouNom, double nouPreu, int novaQuantitat)
+        {
+            
+            int index = -1;
+            for (int i = 0; i < NumElements; i++)
+            {
+                if (productes[i].Equals(producte))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            
+            if (index == -1)
+            {
+                Console.WriteLine("El producte no existeix a la botiga.");
+                return false;
+            }
+
+            // Modifiquem les dades del producte
+            productes[index].Nom = nouNom;
+            productes[index].PreuSenseIva = nouPreu;
+            productes[index].Quantitat = novaQuantitat;
+
+            return true;
+        }
+
+        public void OrdenarProducte()
+        {
+            for (int i = 0; i < NumElements - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < NumElements; j++)
+                {
+                    if (productes[j].Nom.CompareTo(productes[minIndex].Nom) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    Producte temp = productes[i];
+                    productes[i] = productes[minIndex];
+                    productes[minIndex] = temp;
+                }
+            }
+        }
+
+        public void OrdenarPreus()
+        {
+            for (int i = 0; i < NumElements - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < NumElements; j++)
+                {
+                    if (productes[j].PreuSenseIva < productes[minIndex].PreuSenseIva)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    Producte temp = productes[i];
+                    productes[i] = productes[minIndex];
+                    productes[minIndex] = temp;
+                }
+            }
+        }
+
+
+        public bool EsborrarProducte(Producte producte)
+        {
+            int index = -1;
+            for (int i = 0; i < NumElements; i++)
+            {
+                if (productes[i].Equals(producte))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index >= 0)
+            {
+                for (int i = index; i < NumElements - 1; i++)
+                {
+                    productes[i] = productes[i + 1];
+                }
+                NumElements--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Llista de productes:");
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < NumElements; i++)
+            {
+                Console.WriteLine("{0}: {1} ({2} unitats) - Preu sense IVA: {3:C2} | Preu amb IVA: {4:C2}", i + 1, productes[i].Nom, productes[i].Quantitat, productes[i].Preu, productes[i].PreuSenseIva);
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < NumElements; i++)
+            {
+                result += $"{productes[i].Nom}: {productes[i].PreuSenseIva.ToString("C2")} + {Iva.ToString("P0")} = {(productes[i].PreuSenseIva * (1 + Iva)).ToString("C2")}\n";
+            }
+            return result;
+        }
 
 
 
-        //Mètodes
 
 
 
 
 
+
+
+
+        //métodes
     }
 }
 
